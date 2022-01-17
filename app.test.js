6329980882bcdad1bedcad11b0676ebbc6fbc57a
desmond-1 <dayodeji@aellacredit.com>
2022-01-17 13:51:47 +0100

    initial push

---
+++ app.test.js
@@ -0,0 +1,18 @@
+const chaiHttp = require('chai-http');
+const chai, { expect } = require('chai');
+const {fetch} = require('./index');
+chai.use(chaiHttp);
+
+describe('Test the index functions', () => {
+
+    describe('fetch', () => {
+        it('this test fetch() with error-code parameter', (done) => {
+            
+        });
+
+        it('this test fetch() with error-code and error-message parameter', (done) => {
+            
+        });
+    });
+
+});
--- index.js
+++ index.js
@@ -4,30 +4,21 @@ const fs = require('fs');
 const jsonObj = yaml.load(fs.readFileSync(inputfile));
 const AppException = require('./error');
 
-//let errorException = new AppException("4838", "eierur");
 
 const result = {};
     
-const fetch = (code, en = {}) => {
+const fetch = (code, en = 'ng') => {
+
   code = ( typeof(code) == "number" ) ? JSON.stringify(code) : code;
   for(let obj in jsonObj){
     if(obj == code){
       result.code = obj
       result.title = jsonObj[obj]['title']
-      result.message = jsonObj[obj]['message']
+      result.message = jsonObj[obj]['message'][en]
       break;
     }
   }
-
-  if(en != null){
-    console.log(en);
-  }
-  
 }
-
-//fetch('3001');
-
-//console.log(new AppException("4838", "eierur"));
 
 module.exports = {
   fetch,
