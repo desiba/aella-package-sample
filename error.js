6f90730cb5f4d88b61aa3795232652b8b08dbb4c
desiba <desiba_1@yahoo.com>
2022-01-20 07:34:48 +0100

    single directory yml error file

--- error.js
+++ error.js
@@ -12,25 +12,5 @@ class AppException extends Error {
       return this.message;
     }
   }
-  
-  // status-code
-  /*
-  try {
-    throw new AppException(message);
-  } catch(e) {
-    console.error(e);
-    console.error(e.toString());
-    console.log(e.code === 'OUT OF SERVICE');
-  }
-  
-  // status-code and message
-  try {
-    throw new AppException('OUT OF SERVICE', '');
-  } catch(e) {
-    console.error(e);
-    console.error(e.toString());
-    console.log(e.code === 'OUT OF SERVICE');
-  }
-  */
 
   module.exports = AppException
--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const yaml = require('js-yaml');
+const fs = require('fs')
+const crypto = require('crypto');
+const path = require('path')
 const inputfile = './error_template.yml';
-const fs = require('fs');
-const jsonObj = yaml.load(fs.readFileSync(inputfile));
 const AppException = require('./error');
-
 const result = {};
+
+const generateChecksum = (str, algorithm, encoding) => {
+    return crypto.createHash(algorithm || 'md5').update(str, 'utf8').digest(encoding || 'hex');
+};
+
+const config = (option = {}) => {
+  if(option.path !== null){
+    let parsed = fs.readFileSync(option.path, "utf-8");
+    let checksum = generateChecksum(parsed);
+    let fileContent = fs.readFileSync('loaded_hashed_file.txt').toString().split("\n");
+    if(!fileContent.includes(checksum)){
+      fs.appendFileSync('loaded_hashed_file.txt', "\n"+checksum);
+      parsed.split(/\r?\n/).forEach(line => {
+        fs.appendFileSync('error_template.yml', "\n"+line, function (err) {
+          if (err) throw err;
+        })
+      })
+    }
+  }
+}
 
 const fetch = (code, en = 'ng') => {
 
+  //config({ path: path.join(__dirname, 'xyz.yml') });
+
+  const jsonObj = yaml.load(fs.readFileSync(inputfile));
   code = ( typeof(code) == "number" ) ? JSON.stringify(code) : code;
   for(let obj in jsonObj){
     if(obj == code){
@@ -21,6 +44,7 @@ const fetch = (code, en = 'ng') => {
 
 module.exports = {
   fetch,
+  config,
   AppException
 }
 
